c3106ad1c9a347d17e23c1e04cb97a4e4fd7ff1a
Joy Jean <joyjean09@gmail.com>
2016-07-15 19:01:03 -0700

    added last name html, test, and part of password's js and test

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ myApp.controller('FormCtrl', ['$scope', function($scope){
         console.log('$valid', form.birthdate.$valid)
         console.log('$error', form.birthdate.$error)
         validateDOB();
+        confirmPassword();
         // if(form.$valid) {
         // 	window.alert('Payment info has been submitted!');
         // }
@@ -19,11 +20,21 @@ myApp.controller('FormCtrl', ['$scope', function($scope){
         var dob = document.forms["signUpForm"]["birthdate"].value;
         var pattern =/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
         if (dob == null || dob == "" || !pattern.test(dob)) {
-            errMessage += "Invalid date of birth\n";
             return false;
         }
         else {
             return true
         }
     }    
+    
+    function confirmPassword() {
+        var same = (passwordConfirm.value === password.value);
+        if(same) {
+            $scope.signUpForm.passwordConfirm.$setValidity('passwordConfirm', true);
+        } else {
+            $scope.signUpForm.passwordConfirm.$setValidity('passwordConfirm', false);
+            //above works
+            //document.getElementById("#password-confirm").innerHTML = "passwords do not match";
+        }
+    }
 }]);
