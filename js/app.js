4185666445e835b9e97e8763525c0b9042d47814
Jessie Kuo <kuoy2@uw.edu>
2016-07-15 19:01:03 -0700

    index.html, app.js, signup-spec.js updated

--- js/app.js
+++ js/app.js
@@ -4,26 +4,25 @@ var myApp = angular.module('SignUpApp', []);
 
 myApp.controller('FormCtrl', ['$scope', function($scope){
     $scope.submitForm = function(form){
-
-        console.log('$valid', form.birthdate.$valid)
-        console.log('$error', form.birthdate.$error)
-        validateDOB();
-        // if(form.$valid) {
-        // 	window.alert('Payment info has been submitted!');
-        // }
-        // else {
-        // 	window.alert('There was an error submitting your payment. Please check that your information is correct.');
-        // }
+          
     };
-    function validateDOB() {
+    
+    $scope.validateDOB = function() {
         var dob = document.forms["signUpForm"]["birthdate"].value;
-        var pattern =/^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-        if (dob == null || dob == "" || !pattern.test(dob)) {
-            errMessage += "Invalid date of birth\n";
+        console.log(dob);
+        var split = dob.split('/');
+        console.log(split);
+
+        var age = 2016 - split[2];
+        console.log(age);
+        if (age < 13) {
+            $scope.signUpForm.birthdate.$setValidity('birthdate', false);
             return false;
         }
         else {
+            $scope.signUpForm.birthdate.$setValidity('birthdate', true);
             return true
         }
-    }    
+    } 
+
 }]);
--- test/signup-spec.js
+++ test/signup-spec.js
@@ -1,2 +1,29 @@
 //sidnup-spec.js
 'use strict';
+
+beforeEach(function() {
+    browser.refresh();
+});
+
+describe('Birthdate', function() {
+    browser.get('http://localhost:8080/index.html');
+    var age = element(by.css('#birthdate'));
+    var error = element(by.css('#birth-date'));
+    
+    it('should display error message if age not over 13', function() { 
+        age.sendKeys('10/25/2009');
+        expect(error.isDisplayed()).toBe(true);
+
+    });
+
+    it('should not display error message if over 13', function() {
+        age.sendKeys('10/25/1995');
+        expect(error.isDisplayed()).toBe(false);
+
+    });
+
+    it('should display error message if not a date', function() {
+        age.sendKeys(' ');
+        expect(error.isDisplayed()).toBe(true);
+    });
+});
